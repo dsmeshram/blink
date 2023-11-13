de73c8d5762326c44e599cf27466bbfec4ba15b6
damodhar-meshram+1-at-595987277759 <damodhar.meshram@kitaboo.com>
2023-11-14 00:11:09 +0530

    add token from env

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -13,7 +13,7 @@ export const POST = async (req: Request, res : Response) => {
     const {name, email} = await req.json()
     try{
         var user = await addUser(name, email);
-        const token = jwt.sign({email,user}, "JWT_SECRET",  { expiresIn: "1 day" });
+        const token = jwt.sign({email,user}, process.env.JWT_SECRET,  { expiresIn: "1 day" });
         return NextResponse.json({status: 200, token : token})
     } catch(err){
         console.log(err)
